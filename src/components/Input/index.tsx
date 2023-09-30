856321c06205fb4e342228533e74398c6fab1b41
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2023-09-30 12:39:35 -0300

    Add files via upload

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,24 +2,21 @@ import { Center, Checkbox, Grid, GridItem, Input } from "@chakra-ui/react"
 import {MdOutlineAddCircleOutline} from 'react-icons/md'
 import ButtonPers from "../Button"
 
-/*
 interface Props{
-    type: 'checkbox' | 'color'| 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week'
-    placeholder: string
-    value: string
+   margin:string
 } 
-*/
-function InputPersonalidade(){
+
+function InputPersonalidade({margin}:Props){
     return(
 
-        <Center>
+        <Center mb={margin}>
             <Grid templateColumns='repeat(6, 1fr)' gap={6}>
 
-                    <GridItem w='100%' h='10' bg='#bcd2ee' colSpan={3}>
-                        <Input placeholder='Digite o titulo da task.' />
+                    <GridItem w='100%' h='10' bg='#bcd2ee' colSpan={4}>
+                        <Input color="blackAlpha.400" placeholder='Digite o titulo da task.' _placeholder={{ opacity: 1, color: '#fb5012 ',fontSize: ['24px'], }}/>
                     </GridItem>
 
-                    <GridItem colSpan={1.5} >
+                    <GridItem colSpan={1} >
                         <Center>
                             <Checkbox size='md' colorScheme='green' h="40px">
                                 Realizada?
@@ -27,7 +24,7 @@ function InputPersonalidade(){
                         </Center>
                     </GridItem>
                     
-                    <GridItem colSpan={1.5}> 
+                    <GridItem colSpan={1}> 
                         <ButtonPers label="Inserir" leftIcon={<MdOutlineAddCircleOutline />} bg='#23CE6B' variant='solid'/>
                             
                     </GridItem>
